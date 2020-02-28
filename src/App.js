b7f12f7137db0476076122eec51703e0e420b13c
jbathel <jessica.bathel@gmail.com>
2020-02-28 13:11:52 -0800

    Project Setup; Some components in progress and working on routing and links currently

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Cards } from "./components/Cards";
+import { Masthead } from "./components/Masthead";
+import { Footer } from "./components/Footer";
+import { AboutUs } from "./components/AboutUs";
+import { Favorites } from "./components/Favorites";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Route path="/" exact component={Masthead} />
+        <Route path="/favorites" component={Favorites} />
+        <Route path="/about" component={AboutUs} />
+        <Cards />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Masthead.js
@@ -0,0 +1,47 @@
+import React from "react";
+import logo from "../images/lockup.png";
+
+export const Masthead = () => {
+  return (
+    <div>
+      <header className="masthead text-center">
+        <div className="overlay"></div>
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-9 mx-auto mb-2">
+              <img
+                src={logo}
+                alt="Company logo"
+                style={{ width: "30em", padding: "0" }}
+              />
+            </div>
+            <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
+              <form>
+                <div className="form-row">
+                  <div className="col-12 col-md-9 mb-2 mb-md-0">
+                    <input
+                      type="search"
+                      className="form-control form-control-lg"
+                      placeholder="Find your trail..."
+                    />
+                  </div>
+                  <div className="col-12 col-md-3">
+                    <button
+                      type="submit"
+                      className="btn btn-block btn-lg btn-info"
+                    >
+                      Search
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div className="col-md-10 col-md-10 col-md-10 mx-auto">
+              <h1 className="mt-1">Parenting Is An Adventure</h1>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
