31c4ab96a8e4d191c6cbe76ce51ec73992185c4b
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-03-06 10:27:34 -0800

    Added local IP:port to CORS

--- backend/settings.py
+++ backend/settings.py
@@ -80,6 +80,7 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://localhost:5000',
     'http://localhost:8000',
+    'http://127.0.0.1:8000',
 )
 
 ROOT_URLCONF = 'backend.urls'
