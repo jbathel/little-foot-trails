5b6af681e6b31c0f41237f8e900621be8f409503
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-03-05 16:38:47 -0800

    Replaced djangorestframework-jwt with simple_jwt

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from .views import index
 from trail.views import TrailViewSet
@@ -16,7 +19,9 @@ router.register(r'reviews', ReviewViewSet)
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
-    path('token-auth/', obtain_jwt_token),
-    path('user/', include('user.urls')),
     path('api/', include(router.urls)),
+    path('api/token/',
+         TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/',
+         TokenRefreshView.as_view(), name='token_refresh'),
 ]
