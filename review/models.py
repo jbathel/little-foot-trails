91dda1909d116d4664471bacafe19030096b1b62
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-03-09 19:14:22 -0700

    Raised max characters for Review description to 1250

--- review/models.py
+++ review/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 class Review(models.Model):
     name = models.CharField(max_length=30)
-    review = models.CharField(max_length=250)
+    review = models.CharField(max_length=1250)
     stars = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
