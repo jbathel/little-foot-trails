452a795202269ab915647ca9bebc38112b72b95b
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-03-12 18:14:09 -0700

    Uncommented line 29 with the re_path in urls

--- backend/urls.py
+++ backend/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
          TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/',
          TokenRefreshView.as_view(), name='token_refresh'),
-   # re_path(r'^(?:.*)/?$', index, name='index'),
+   re_path(r'^(?:.*)/?$', index, name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
